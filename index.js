6ef484ca17180a82d2e719fde851f2c46c989873
gfttrt <fsjcirecapeplox@hotmail.com>
2024-05-16 04:08:54 +0700

    update sdk

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
-const colors = ["red", "green", "blue"];
-const randomColor = colors[Math.floor(Math.random() * colors.length)];
-console.log(randomColor);
+function longestCommonPrefix(strs) {
+  if (strs.length === 0) return "";
+  let prefix = strs[0];
+  for (let i = 1; i < strs.length; i++) {
+    let j = 0;
+    while (
+      j < prefix.length &&
+      j < strs[i].length &&
+      prefix.charAt(j) === strs[i].charAt(j)
+    ) {
+      j++;
+    }
+    prefix = prefix.substring(0, j);
+  }
+  return prefix;
+}
