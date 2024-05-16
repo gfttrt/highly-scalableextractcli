3afce1cdc46b1697aad5a4848ae577401b7bdead
gfttrt <fsjcirecapeplox@hotmail.com>
2024-05-16 13:01:08 +0700

    aptos project

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
-function longestCommonPrefix(strs) {
-  if (strs.length === 0) return "";
-  let prefix = strs[0];
-  for (let i = 1; i < strs.length; i++) {
-    let j = 0;
-    while (
-      j < prefix.length &&
-      j < strs[i].length &&
-      prefix.charAt(j) === strs[i].charAt(j)
-    ) {
-      j++;
-    }
-    prefix = prefix.substring(0, j);
+function longestPalindromeSubseq(s) {
+  const n = s.length;
+  const dp = Array.from(Array(n), () => Array(n).fill(0));
+  for (let i = 0; i < n; i++) {
+    dp[i][i] = 1;
   }
-  return prefix;
+  for (let len = 2; len <= n; len++) {
+    for (let i = 0; i < n - len + 1; i++) {
+      const j = i + len - 1;
+      if (s[i] === s[j]) {
+        dp[i][j] = 2 + dp[i + 1][j - 1];
+      } else {
+        dp[i][j] = Math.max(dp[i + 1][j], dp[i][j - 1]);
+      }
+    }
+  }
+  return dp[0][n - 1];
 }
